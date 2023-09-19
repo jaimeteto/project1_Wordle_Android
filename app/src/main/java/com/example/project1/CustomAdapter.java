5cecaf3323c3c8586c003fd5821967357745c0c1
jaimeteto <100324512+jaimeteto@users.noreply.github.com>
2023-09-18 22:52:31 -0400

    second commit. onclick listener and ontextchange were implemented so that both grids(button grid and textEdit grid can interact with each other

--- app/src/main/java/com/example/project1/CustomAdapter.java
+++ app/src/main/java/com/example/project1/CustomAdapter.java
@@ -1,56 +1,107 @@
 package com.example.project1;
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 public class CustomAdapter extends BaseAdapter {
     Context context;
     LayoutInflater inflater;
     char[] inputs;
     int gridNum;
-    public CustomAdapter(Context applicationContext, char[] inputs,int gridNum) {
+    GridView grid;
+
+
+    public CustomAdapter(Context applicationContext, char[] inputs, int gridNum, GridView grid) {
         this.context = applicationContext;
-        this.gridNum=gridNum;
-        this.inputs=inputs;
+        this.gridNum = gridNum;
+        this.inputs = inputs;
+        this.grid = grid;
         inflater = (LayoutInflater.from(applicationContext));
     }
+
     @Override
     public int getCount() {
         return inputs.length;
     }
+
     @Override
     public Object getItem(int i) {
         return null;
     }
+
     @Override
     public long getItemId(int i) {
         return 0;
     }
+
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
 
-        if(gridNum==1) {
+        if (gridNum == 1) {
             view = inflater.inflate(R.layout.input, null); // inflate the layout
             //get the view
 
             EditText input1 = (EditText) view.findViewById(R.id.input); // get the reference of ImageView
-            input1.setText("__");
+            input1.addTextChangedListener(new TextWatcher() {
 
-        } else if (gridNum==2) {
+                public void afterTextChanged(Editable s) {}
+
+                public void beforeTextChanged(CharSequence s, int start,
+                                              int count, int after) {
+                }
+
+                public void onTextChanged(CharSequence s, int start,
+                                          int before, int count) {
+                    //change focus to the following edit text
+                    int nextEditText = i+1;
+                    if(nextEditText<=inputs.length){
+                        EditText next = ((LinearLayout) grid.getChildAt(nextEditText)).findViewById(R.id.input);
+                        next.requestFocus();
+                    }
+
+                    }
+
+
+            });
+
+
+        } else if (gridNum == 2) {
             view = inflater.inflate(R.layout.button, null); // inflate the layout
             //get the view
 
-            Button myButton = (Button) view.findViewById(R.id.aButton);
-            String sequence= String.valueOf(inputs[i]);
-
+            Button myButton = (Button) view.findViewById(R.id.myButton);
+            String sequence = String.valueOf(inputs[i]);
             myButton.setText(sequence);
+            myButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context.getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+                    String storedChar = ((Button) view).getText().toString();
+                    //EditText current = ((LinearLayout) grid.getChildAt(0)).findViewById(R.id.input);
+                    EditText current = ((LinearLayout) grid.getFocusedChild()).findViewById(R.id.input);
+                    //Toast.makeText(context, "Item was pressed ", Toast.LENGTH_SHORT).show();
+                    current.setText(storedChar);
 
+                }
+
+
+            });
 
         }
+
+        // myButton.setText(sequence);
+
+
         return view;
     }
 }
--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -9,35 +9,45 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     private Button aButton2;
-    GridView inputGrid;
-    GridView buttonGrid;
-    char inputs[]={'1','2','3','4','5','1','2','3','4','5','1','2','3','4','5'};
+    public GridView inputGrid;
+    public GridView buttonGrid;
+    char inputs[]={'1','2','3','4','5','1','2','3','4','5','1','2','3','4','5','1','2','3','4','5','1','2','3','4','5'};
     char[] letters={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     EditText focusedEditText;
+
+    public int inputLocation =0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //aButton2 = findViewById(R.id.aButton);
         //aButton2.setOnClickListener(this);
-
+        inputLocation = 0;
         inputGrid = (GridView) findViewById(R.id.grid1); // init GridView
         buttonGrid = (GridView) findViewById(R.id.grid2); // init GridView
         // Create an object of CustomAdapter and set Adapter to GirdView
-        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(),inputs,1);
-        CustomAdapter customAdapter2 = new CustomAdapter(getApplicationContext(),letters,2);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), inputs, 1,inputGrid);
+        CustomAdapter customAdapter2 = new CustomAdapter(getApplicationContext(), letters, 2,inputGrid);
         inputGrid.setAdapter(customAdapter);
         buttonGrid.setAdapter(customAdapter2);
+//        buttonGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                //on click get the letter of the button that was pressed and set the current focused edit text
+//                String storedChar = ((Button) view).getText().toString();
+//                EditText current = (EditText) inputGrid.getChildAt(inputLocation);
+//                Toast.makeText(getApplicationContext(), "Item was pressed ", Toast.LENGTH_SHORT).show();
+//                // current.setText("H");
+//                inputLocation++;
+//
+//            }
+//        });
+
     }
-    @Override
-    public void onClick(View view){
 
-        EditText nextView;
-        EditText currentView;
-
-
-    }
 }
